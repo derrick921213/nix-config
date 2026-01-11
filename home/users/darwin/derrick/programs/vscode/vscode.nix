9e14630b6b2ace0cac24bdd3725d644095cd8efc
derrick921213 <dlin12457@gmail.com>
2026-01-11 14:27:47 +0800

    Fix: vscode options

--- home/users/darwin/derrick/programs/vscode/vscode.nix
+++ home/users/darwin/derrick/programs/vscode/vscode.nix
@@ -8,7 +8,7 @@
       publisher = "ankitcode";
       name = "firefly";
       version = "4.0.0";
-      sha256 = lib.fakeSha256;
+      sha256 = "sha256-CUu7GHp4ks0y4X0UuAA5bROU2Mc2BJHQttmq4gcv1wY=";
     };
   };
   path-autocomplete = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
@@ -16,7 +16,7 @@
       publisher = "ionutvmi";
       name = "path-autocomplete";
       version = "1.25.0";
-      sha256 = lib.fakeSha256;
+      sha256 = "sha256-iz32o1znwKpbJSdrDYf+GDPC++uGvsCdUuGaQu6AWEo=";
     };
   };
   path-intellisense = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
@@ -24,35 +24,35 @@
       publisher = "christian-kohler";
       name = "path-intellisense";
       version = "2.10.0";
-      sha256 = lib.fakeSha256;
+      sha256 = "sha256-bE32VmzZBsAqgSxdQAK9OoTcTgutGEtgvw6+RaieqRs=";
     };
   };
 in {
   programs.vscode = {
     enable = true;
-    # vscode 全域設定
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      ms-ceintl.vscode-language-pack-zh-hant
-      editorconfig.editorconfig
-      mikestead.dotenv
-      firefly
-      pkief.material-icon-theme
-      mhutchie.git-graph
-      oderwat.indent-rainbow
-      path-autocomplete
-      path-intellisense
-    ];
-    userSettings = {
-      "editor.fontFamily" = "JetBrainsMono Nerd Font";
-      "editor.fontSize" = 14;
-      "files.autoSave" = "afterDelay";
-      "editor.formatOnSave" = true;
-    };
     profiles = {
+      default = {
+        # vscode 全域設定
+        extensions = with pkgs.vscode-extensions; [
+          vscodevim.vim
+          ms-ceintl.vscode-language-pack-zh-hant
+          editorconfig.editorconfig
+          mikestead.dotenv
+          firefly
+          pkief.material-icon-theme
+          mhutchie.git-graph
+          oderwat.indent-rainbow
+          path-autocomplete
+          path-intellisense
+        ];
+        userSettings = {
+          "editor.fontFamily" = "JetBrainsMono Nerd Font";
+          "editor.fontSize" = 14;
+          "files.autoSave" = "afterDelay";
+          "editor.formatOnSave" = true;
+        };
+      };
       Rust = {
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
         extensions = with pkgs.vscode-extensions; [
           rust-lang.rust-analyzer
           tamasfe.even-better-toml
@@ -67,8 +67,6 @@ in {
         };
       };
       Nix = {
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
         extensions = with pkgs.vscode-extensions; [
           kamadorueda.alejandra
           bbenoist.nix
