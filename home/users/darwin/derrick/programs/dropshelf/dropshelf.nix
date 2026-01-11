84623eed1684377acb65e3a54930cded717b0705
derrick921213 <dlin12457@gmail.com>
2026-01-11 22:31:53 +0800

    Add: dropshelf

---
+++ home/users/darwin/derrick/programs/dropshelf/dropshelf.nix
@@ -0,0 +1,27 @@
+{
+  stdenv,
+  lib,
+  unzip,
+  ...
+}:
+stdenv.mkDerivation {
+  pname = "dropshelf";
+  version = "1729";
+
+  src = ./Dropshelf-build-1729.zip;
+
+  nativeBuildInputs = [unzip];
+
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    cp -R *.app $out/Applications/
+  '';
+
+  meta = with lib; {
+    platforms = platforms.darwin;
+  };
+}
