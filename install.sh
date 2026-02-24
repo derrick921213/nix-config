87007600396d861a304cf24621865f4d3b4369c8
derrick921213 <dlin12457@gmail.com>
2026-02-24 20:16:58 +0800

    新增安裝腳本以支援 NixOS 配置

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo nix run --experimental-features "nix-command flakes" \
+  github:nix-community/disko#disko-install -- \
+  --flake .#nixos-comp
+#nix run github:nix-community/disko -- --mode disko-install .#nixos-comp
