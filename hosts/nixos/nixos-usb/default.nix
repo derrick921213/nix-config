1dcf596768e2306997a5dadc7bf555fbc4f5f229
derrick921213 <dlin12457@gmail.com>
2026-02-24 09:23:06 +0800

    add nixos-usb

---
+++ hosts/nixos/nixos-usb/default.nix
@@ -0,0 +1,174 @@
+{
+  config,
+  lib,
+  pkgs,
+  hostname,
+  ...
+}: let
+  isX86_64 = pkgs.stdenv.hostPlatform.isx86_64;
+in {
+  boot = {
+   loader = {
+     systemd-boot.enable = true;
+     efi.canTouchEfiVariables = false;
+   };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = lib.mkIf isX86_64 true;
+  };
+
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Asia/Taipei";
+
+  i18n.defaultLocale = "zh_TW.UTF-8";
+  
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    vista-fonts
+    corefonts
+  ];
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  security.rtkit.enable = true;
+  security.pam.services.hyprlock = {};
+  security.pam.services.greetd.kwallet = {
+    enable = true;
+    package = pkgs.kdePackages.kwallet-pam;
+  };
+
+  services.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
+  virtualisation.vmware.guest = {
+    enable = true;
+    headless = false;
+  };
+
+  users.users.derrick = {
+    isNormalUser = true;
+    description = "derrick";
+    extraGroups = ["networkmanager" "wheel" "video" "audio"];
+    hashedPassword = "$6$imjFfK.gjd3R5Tb3$0qqFQPLqUZ9oFP38736p3Q.VZz9c0w1Uepdbf6.ulXhWJx1gcgO6zL0pYQqlCHmjPYTflm3wIT4eTswkvjNxf1";
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCq3fqTChvwq6LS6MgIhUHtIAeTIt8NYtWLWeFM4fv0uqHBR8RGBpQICxUmhfuW4cIX3DfCTSlrmgStjQKJUGfR9LHHAJZfRc7eKlWDmj4h6Pfez+cv+dZp7i6FW4PbbKa5u0DnZqoUcjqsPBAW/xnNrKuFso2GBQDVL/ynvvwp/8o+wHUR5f4n6Gshy2uFTx35gtEXLMd/BA+I7scKch11R2QwYwWoTicl2ZIVYrS3H8fCmL/ecNaYuxAv/ilD4JPbthM3zxhid6CV/yR5cDG0slit69NX3EmHBmvmoPzRF0b6OICOfn7aqEqAk/mMkxrpRMpqC53QwRATRwWzDuzqd7NiLV9hVZ+uAuT6Dp/1BzgX8iEVN7rGsMKa5mdq+mlSdJ6dNeXKbgpTw931yz5aYp6pn1sBaWhP0dqqe34xddsmeJ5MBYSGkgyfz0+0gwErZ0UgryZ/i8zFKJfj4qf2EHzTyBfYGSPQ8JphCYL74n96ElpcFrEDVJK5FFOJUQ8= derrick@derrickdeMacBook-Pro.local"
+    ];
+  };
+
+  security.sudo = {
+    enable = true;
+    execWheelOnly = false;
+    wheelNeedsPassword = true;
+    extraRules = [
+      {
+        users = ["derrick"];
+        host = "ALL";
+        runAs = "ALL:ALL";
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
+
+  nix.settings.trusted-users = [
+    "root"
+    "derrick"
+    "@wheel"
+  ];
+
+  environment.systemPackages =
+    (with pkgs; [
+      vim
+      wget
+      brightnessctl
+      pavucontrol
+      pasystray
+      pamixer
+      stirling-pdf
+      affine
+      xournalpp
+      kdePackages.okular
+      kdePackages.gwenview
+      kdePackages.kwave
+      kdePackages.k3b
+      kdePackages.kwallet
+      kdePackages.kwallet-pam
+      kdePackages.kwalletmanager
+      nomacs
+      vlc
+      audacity
+      dust
+      ncdu
+      atool
+      qbittorrent-enhanced
+      rclone
+      rclone-ui
+      rclone-browser
+      bleachbit
+      hardinfo2
+      localsend
+      clamav
+      clamtk
+      networkmanagerapplet
+      file
+      xdg-utils
+      shared-mime-info
+      kdePackages.polkit-kde-agent-1
+      libsecret
+    ])
+    ++ lib.optionals isX86_64 (with pkgs; [
+      bottles
+      lutris
+    ]);
+
+  services.resolved.enable = true;
+  services.udev.packages = [pkgs.brightnessctl pkgs.networkmanagerapplet];
+  # services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableKwallet = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+    };
+  };
+  programs.nix-ld.dev.enable = true;
+  system.stateVersion = "25.11";
+}
---
+++ hosts/nixos/nixos-usb/meta.nix
@@ -0,0 +1,33 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  system = "x86_64-linux";
+  user = "derrick";
+  hostip = "172.16.125.144";
+  remoteBuild = true;
+  diskDevice = "/dev/sda";
+  extraModules = [
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image.nix>
+    (self + "/modules/filewall/firewall.nix")
+    (self + "/modules/desktop/greetd.nix")
+    (self + "/modules/desktop/hyprland.nix")
+    (self + "/modules/desktop/qtile.nix")
+    (self + "/modules/filemanager/dolphin.nix")
+    (self + "/modules/virtualisation/docker.nix")
+    (self + "/modules/virtualisation/virt-qemu.nix")
+    # inputs.disko.nixosModules.disko
+    inputs.nix-ld.nixosModules.nix-ld
+    # ./disko.nix
+    ./hardware-configuration.nix
+  ];
+  firewall-tags = ["ssh" "web"];
+  pkgsChannel = "stable";
+  deployment = {
+    targetHost = "172.16.125.144";
+    targetUser = "derrick";
+    targetPort = 22;
+    buildOnTarget = true;
+  };
+}
