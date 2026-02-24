6f542730065c9753d37859f01f3aa2f00384108e
derrick921213 <dlin12457@gmail.com>
2026-02-25 06:59:04 +0800

    更新安裝腳本，新增空行並修正註解以提高可讀性

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 sudo nix run --experimental-features "nix-command flakes" \
   github:nix-community/disko#disko-install -- \
   --flake .#nixos-comp
+
+
 #nix run github:nix-community/disko -- --mode disko-install .#nixos-comp
-#nix run --experimental-features "nix-command flakes" github:nix-community/nixos-anywhere -- --build-on-remote --flake .#nixos-comp root@172.16.125.143
+#nix run --experimental-features "nix-command flakes" github:nix-community/nixos-anywhere -- --build-on-remote --flake .#nixos-comp --generate-hardware-config nixos-generate-config ./hosts/nixos/nixos-comp/hardware-configuration.nix root@172.16.125.143
