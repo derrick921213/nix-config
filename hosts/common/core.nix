7bb2c19ea3f7904d4419b82c553711bdf11f2d9d
derrick921213 <dlin12457@gmail.com>
2026-03-01 16:42:32 +0800

    新增 hyprland.cachix.org 至 substituters，以擴展 Nix 環境的緩存來源

--- hosts/common/core.nix
+++ hosts/common/core.nix
@@ -14,6 +14,7 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
