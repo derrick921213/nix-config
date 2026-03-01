917987ff880e88f4da3428e81388f8beb685b6ed
derrick921213 <dlin12457@gmail.com>
2026-03-01 16:32:52 +0800

    新增 nix-community.cachix.org 的信任公鑰和替代者，以增強 Nix 環境的安全性

--- hosts/common/core.nix
+++ hosts/common/core.nix
@@ -13,6 +13,11 @@
       trusted-users = ["root" "@admin" "derrick"];
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
     gc = {
