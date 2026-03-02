58e73a0defb9e4c4ce4dbfbce514254da2a8dc88
derrick921213 <dlin12457@gmail.com>
2026-03-02 14:51:24 +0800

    Fix: update Docker configuration to remove rootless settings and set DNS

--- modules/virtualisation/docker.nix
+++ modules/virtualisation/docker.nix
@@ -8,9 +8,8 @@
     enable = true;
     enableOnBoot = true;
     autoPrune.enable = true;
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
+    daemon.settings = {
+      dns = ["1.1.1.1" "8.8.8.8"];
     };
   };
 
