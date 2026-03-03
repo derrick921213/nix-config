3352a5cf73a3741fc5c7e6f7ae0f447b7fbd2c89
derrick921213 <dlin12457@gmail.com>
2026-03-03 10:26:45 +0800

    Disable zathura

--- hosts/nixos/nixos-comp/default.nix
+++ hosts/nixos/nixos-comp/default.nix
@@ -187,7 +187,9 @@ in {
   services.resolved.enable = true;
   services.udev.packages = [pkgs.brightnessctl pkgs.networkmanagerapplet];
   programs.seahorse.enable = true;
-  # services.gnome.gnome-keyring.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+  programs.dconf.enable = true;
   security.pam.services.greetd.enableKwallet = true;
   security.pam.services.login.kwallet.enable = true;
   environment.variables = {
