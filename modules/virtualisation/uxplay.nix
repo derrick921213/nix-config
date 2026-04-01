e47e3eca5c5b0e857ef1277d2ef688da78287def
derrick921213 <dlin12457@gmail.com>
2026-04-01 12:16:21 +0800

    Add: restructure uxplay configuration for improved service management

--- modules/virtualisation/uxplay.nix
+++ modules/virtualisation/uxplay.nix
@@ -1,4 +1,16 @@
-{pkgs,...}: {
+{pkgs, ...}: {
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+      domain = true;
+    };
+  };
   environment.systemPackages = [pkgs.uxplay];
   services.avahi.enable = true;
   services.avahi.nssmdns4 = true;
