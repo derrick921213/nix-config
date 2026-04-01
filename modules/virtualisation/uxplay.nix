f0462d5da33266723f12810b9dbfbcfd5d73e0d0
derrick921213 <dlin12457@gmail.com>
2026-04-01 13:34:40 +0800

    Add: include Airplay icon in uxplay desktop item configuration

--- modules/virtualisation/uxplay.nix
+++ modules/virtualisation/uxplay.nix
@@ -1,5 +1,15 @@
 {pkgs, ...}: {
-  environment.systemPackages = [pkgs.uxplay];
+  environment.systemPackages = [
+    pkgs.uxplay
+    (makeDesktopItem {
+      name = "uxplay-custom";
+      desktopName = "iOS Mirror";
+      exec = "uxplay -p -n \"Derrick-NixOS\"";
+      icon = "~/.config/icon/Airplay.png";
+      terminal = false;
+      categories = ["Utility"];
+    })
+  ];
   networking.firewall.allowedTCPPorts = [7000 7001 7100];
   networking.firewall.allowedUDPPorts = [5353 6000 6001 7011];
 }
