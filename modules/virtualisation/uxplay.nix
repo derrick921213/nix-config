dae0dc5997ff5c53e7d38b26dd4356a5cdf687a9
derrick921213 <dlin12457@gmail.com>
2026-04-01 12:18:48 +0800

    Refactor: remove Avahi service configuration from uxplay module

--- modules/virtualisation/uxplay.nix
+++ modules/virtualisation/uxplay.nix
@@ -1,7 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.uxplay];
-  services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
   networking.firewall.allowedTCPPorts = [7000 7001 7100];
   networking.firewall.allowedUDPPorts = [5353 6000 6001 7011];
 }
