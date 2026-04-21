fc6ba3d16dd8d4092adb502977daf60a0d2bd350
derrick921213 <dlin12457@gmail.com>
2026-04-21 09:56:08 +0800

    Add: duplicate SSH configuration for local IP address 192.168.1.4

--- home/programs/ssh/ssh.nix
+++ home/programs/ssh/ssh.nix
@@ -9,6 +9,12 @@
         identityFile = "~/.ssh/id_ed25519";
         identitiesOnly = true;
       };
+      "192.168.1.4" = {
+        hostname = "192.168.1.4";
+        user = "derrick";
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+      };
       "github.com-out" = {
         hostname = "github.com";
         user = "git";
